8ee184d20b37c2b9e9e7ca37eb4515871e689d9b
ahmetkcak <ahmet.aliberk07@gmail.com>
2024-10-07 12:22:35 +0300

    added data control for exportToExcel func

--- medas-demo-client/src/utils/exportToExcel.js
+++ medas-demo-client/src/utils/exportToExcel.js
@@ -1,9 +1,14 @@
+import { toast } from 'react-toastify';
 import * as XLSX from 'xlsx';
 
 const exportToExcel = (data, headers, sheetName, fileName) => {
+    if (data.length === 0) {
+        toast.error('No data available for export.');
+        return;
+    }
     const ws = XLSX.utils.aoa_to_sheet([headers, ...data]);
     const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, sheetName);    
+    XLSX.utils.book_append_sheet(wb, ws, sheetName);
     XLSX.writeFile(wb, fileName);
 };
 
